8e41b3bf780219bc57947980aacdf3351e4ec5da
Johanna <jscarlberg@gmail.com>
2018-03-15 13:01:52 +0000

    building out the GUI

--- src/ojaace6/logic/ButtonFrame.java
+++ src/ojaace6/logic/ButtonFrame.java
@@ -1,11 +1,16 @@
 package ojaace6.logic;
 
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class ButtonFrame extends JFrame {
+	private JButton button;
+	private JLabel label;
+	
 	private static final int FRAME_WIDTH = 100;
 	private static final int FRAME_HEIGHT = 60;
 	
@@ -15,11 +20,23 @@ public class ButtonFrame extends JFrame {
 		setSize(FRAME_WIDTH, FRAME_HEIGHT); 
 	}
 	
+	class ClickListener implements ActionListener
+	{
+		public void actionPerformed(ActionEvent event) 
+		{
+			label.setText("I was clicked."); 
+		}
+	}
+	
 	private void createComponents() 
 	{
-		JButton button = new JButton("Click me!"); JPanel panel = new JPanel(); panel.add(button);
-		add(panel);
+		button = new JButton("Click me!");
 		ActionListener listener = new ClickListener();
-		button.addActionListener(listener); 
+		button.addActionListener(listener);
+		label = new JLabel("Hello You World!");
+		JPanel panel = new JPanel(); 
+		panel.add(button);
+		panel.add(label);
+		add(panel);
 	}
 }
--- src/ojaace6/logic/ClickListener.java
+++ src/ojaace6/logic/ClickListener.java
@@ -3,8 +3,6 @@ package ojaace6.logic;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.JButton;
-
 public class ClickListener implements ActionListener {
 	public void actionPerformed(ActionEvent event) 
 	{
